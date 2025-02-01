49ebbf47f509afa1c511a536746f4aa4e8734e29
rockstarsolider <pouriaghiasi3@gmail.com>
2025-02-01 18:13:59 +0330

    debug = true

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
