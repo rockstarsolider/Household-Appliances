e52f03d903e3b1df1e4cddc21d16ded2669177d4
rockstarsolider <pouriaghiasi3@gmail.com>
2025-04-21 17:29:45 +0330

    search functionality

--- cart/admin.py
+++ cart/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import CartItem, Order
 
 # Register your models here.
-admin.site.register(CartItem)
-admin.site.register(Order)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['user', 'order', 'product', 'quantity',]
+    search_fields = ['user', 'order', 'product', 'quantity',]
+    list_filter = ['user', 'order', 'product']
+    list_per_page = 50
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'formatted_price', 'order_date', 'transaction_id']
+    search_fields = ['user', 'name', 'formatted_price', 'order_date', 'transaction_id']
+    list_filter = ['user', 'order_date']
+    list_per_page = 50
+
+    @admin.display(description='قیمت کل')  
+    def formatted_price(self, obj):  
+        return f"{obj.total_price:,} تومان"
+
+admin.site.register(CartItem, CartItemAdmin)
+admin.site.register(Order, OrderAdmin)
--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import CustomUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'phone_number', 'name', 'email',]
+    search_fields = ['phone_number', 'id', 'name', 'email',]
+    list_display_links = ['phone_number']
+    list_filter = ['is_staff', 'date_joined']
+    list_per_page = 50
+
+admin.site.register(CustomUser, CustomUserAdmin)
